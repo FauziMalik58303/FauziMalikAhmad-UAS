b7b255df9c41a74ce1a73af1f1b70e35ce391828
Fauzi malik <uzieahmad1002@gmail.com>
2025-06-30 01:26:41 +0800

    Update settings.py

--- myjob/settings.py
+++ myjob/settings.py
@@ -114,8 +114,12 @@ DATABASES = {
         'PASSWORD': os.getenv('MYSQL_PASSWORD', ''),
         'HOST': os.getenv('MYSQL_HOST', '127.0.0.1'),
         'PORT': os.getenv('MYSQL_PORT', '3306'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION'",
+        },
     }
 }
+
 
 
 
