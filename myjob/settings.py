92043782a2c6a1375b8a2f3426302ee3e4cb2d33
Fauzi malik <uzieahmad1002@gmail.com>
2025-06-30 21:16:52 +0800

    update screet kay, debug true,

--- myjob/settings.py
+++ myjob/settings.py
@@ -34,10 +34,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o0%9lcpu7t(5q#%-cd^t$iuhvr3g=8bw(=k)q51szj@bs41r(6'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key-for-dev-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() in ['true', '1', 't']
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'fauzimalikahmad-uas-production.up.railway.app']
 
