74e64c15dd7032e822b576f4bb8ef6525be50fa3
Fauzi malik <uzieahmad1002@gmail.com>
2025-07-01 05:43:13 +0800

    add manage.py

--- manage.py
+++ manage.py
@@ -3,24 +3,6 @@
 import os
 import sys
 
-def create_admin():
-    import django
-    from django.contrib.auth import get_user_model
-
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myjob.settings')
-    django.setup()
-
-    User = get_user_model()
-    username = 'fauzimalik'
-    password = 'fz0881'
-    email = 'uziehamd1002@gmail.com'
-
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
-        print(f'Superuser "{username}" berhasil dibuat.')
-    else:
-        print(f'Superuser "{username}" sudah ada, tidak dibuat ulang.')
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myjob.settings')
@@ -32,11 +14,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-
-    # Jalankan hanya saat migrate atau runserver dijalankan
-    if len(sys.argv) > 1 and sys.argv[1] in ['runserver', 'migrate', 'runserver_plus']:
-        create_admin()
-
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
