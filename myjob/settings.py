8012b12c82724fbc2314a289b9e728ed7d11e55e
Fauzi malik <uzieahmad1002@gmail.com>
2025-06-30 18:44:47 +0800

    update setings

--- myjob/settings.py
+++ myjob/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o0%9lcpu7t(5q#%-cd^t$iuhvr3g=8bw(=k)q51szj@bs41r(6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'fauzimalikahmad-uas-production.up.railway.app']
 
